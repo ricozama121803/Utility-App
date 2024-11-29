509bd3e4a9f23dbe0f7aca2b1592c544751fc07f
Enrico Zamarripa <ricozama121803@gmail.com>
2024-11-29 09:32:31 -0800

    backend ready for reqs

--- demos/ecommerce_shop/backend/routes/file.ts
+++ demos/ecommerce_shop/backend/routes/file.ts
@@ -6,6 +6,14 @@ import cors from "cors";
 const router = express.Router();
 
 router.get("/api/tokens-file", cors(), (req, res) => {
+    const apiKey = req.headers['x-api-key'];
+    console.log('Received API Key:', apiKey);
+    console.log('Expected API Key:', process.env.API_SECRET_KEY);
+
+    if (!apiKey || apiKey !== process.env.API_SECRET_KEY) {
+        return res.status(401).json({ error: 'Invalid API key' });
+    }
+
     try {
       const filePath = path.join(__dirname, '..', 'canva-tokens.json');
       const fileContent = fs.readFileSync(filePath, 'utf8');
@@ -15,6 +23,7 @@ router.get("/api/tokens-file", cors(), (req, res) => {
       res.status(500).json({ error: 'Failed to retrieve tokens file' });
     }
 });
+
 
 router.get('/api/tokens', cors(), (req, res) => {
     try {
